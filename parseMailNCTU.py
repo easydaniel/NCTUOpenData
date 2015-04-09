bae23ac53f8a339934b44b1cd334b36427983ecc
easydaniel <whitetiger50512@gmail.com>
2015-04-09 23:39:33 +0800

    gogo

--- parseMailNCTU.py
+++ parseMailNCTU.py
@@ -8,24 +8,26 @@ mailData = []
 formatOfData = ['id','name','date','type']
 
 for url in dormURL:
-	page = urllib.request.urlopen(url)
-	soup = BeautifulSoup(page.read().decode('hkscs'))
-	tables = None
-	try:
-		tables = soup.body.table.find_all('tr')
-	except:
-		continue
-	if tables:
-		dormName = soup.body.center.b.contents[0].replace('領信通知',' ').strip()
-		tables = tables[1:]
-		for t in tables:
-			lines = t.find_all('font')
-			tmpData = {'exist':True,'dorm':dormName}
-			for index in range(0,4):
-				tmpData[formatOfData[index]] = lines[index].contents[0].strip()
-			mailData.append(tmpData)
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read().decode('hkscs'))
+    tables = None
+    try:
+        tables = soup.body.table.find_all('tr')
+    except:
+        continue
+    if tables:
+        dormName = soup.body.center.b.contents[0].replace('領信通知',' ').strip()
+        tables = tables[1:]
+        for t in tables:
+            lines = t.find_all('font')
+            tmpData = {'exist':True,'dorm':dormName}
+            for index in range(0,4):
+                tmpData[formatOfData[index]] = lines[index].contents[0].strip()
+                mailData.append(tmpData)
 
 print(mailData)
+
+		
 
 # mailData = [{'id','name','date','type','exist','dorm'}...]
 
